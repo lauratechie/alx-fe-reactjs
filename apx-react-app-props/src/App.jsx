65c15b4e3433410b3801ad6a4026915b28936cfc
LMaithya <laura.maithya@strathmore.edu>
2025-09-14 23:02:11 +0300

    I am tayyyyaaaaddddd!!!

--- apx-react-app-props/src/App.jsx
+++ apx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext . js';
+import UserContext from './UserContext.js';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
