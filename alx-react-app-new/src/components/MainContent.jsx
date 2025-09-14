add489e04b6afb9c489e505ba2ec521ee8c71208
LMaithya <laura.maithya@strathmore.edu>
2025-09-14 22:06:59 +0300

    Created the alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: '#eef2f3',
+        padding: '20px',
+        margin: '10px',
+        borderRadius: '8px',
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
